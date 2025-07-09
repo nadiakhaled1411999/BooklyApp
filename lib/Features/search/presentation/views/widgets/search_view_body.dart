588b85926ae6cab57871acf172deb74fc8bfb9b3
Nadia khaled <d703403@gmail.com>
2025-07-09 20:04:56 +0300

    done search view part2

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,8 +1,8 @@
-import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
+
 import 'package:bookly_app/Features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:bookly_app/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
-
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -13,35 +13,27 @@ class SearchViewBody extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          SizedBox(height: 15,),
+          SizedBox(
+            height: 15,
+          ),
           CustomSearchTextField(),
-SizedBox(height: 16,),
- Align(
-alignment: Alignment.topLeft,
-  child: Text( "Search Result",style: Styles.textStyle18,)),
-SizedBox(height: 16,),
-
-        Expanded(child:SearchResultListView())],
+          SizedBox(
+            height: 16,
+          ),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Search Result",
+                style: Styles.textStyle18,
+              )),
+          SizedBox(
+            height: 16,
+          ),
+          Expanded(child: SearchResultListView())
+        ],
       ),
     );
   }
 }
 
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-
-        padding: EdgeInsets.zero,
-        scrollDirection: Axis.vertical,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: BookListViewItem(),
-          );
-        });
-  }
-}
