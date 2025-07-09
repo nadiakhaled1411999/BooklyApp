e082c9fc8b0936084501550141213ca0f192c81c
Nadia khaled <d703403@gmail.com>
2025-07-09 19:31:37 +0300

    create custom search text filed

---
+++ lib/Features/search/presentation/views/widgets/custom_search_text_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchTextField extends StatelessWidget {
+  const CustomSearchTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          enabledBorder: buildOutLineInputBorder(),
+          focusedBorder: buildOutLineInputBorder(),
+          hintText: "Search",
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: const Opacity(
+              opacity: 0.8,
+              child: Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 22,
+              ),
+            ),
+          )),
+    );
+  }
+
+  OutlineInputBorder buildOutLineInputBorder() {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: const BorderSide(color: Colors.white),
+    );
+  }
+}
--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,27 +1,18 @@
+import 'package:bookly_app/Features/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class  SearchViewBody extends StatelessWidget {
-  const  SearchViewBody({super.key});
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column( children: [
-CustomSearchTextField()
-    ],);
-  }
-}
-class  CustomSearchTextField extends StatelessWidget {
-  const  CustomSearchTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const TextField(
-      decoration: InputDecoration(
-        hintText:"Search",
-suffixIcon:Icon( FontAwesomeIcons.magnifyingGlass,size: 22,)
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [CustomSearchTextField()],
       ),
-
     );
   }
 }
+
