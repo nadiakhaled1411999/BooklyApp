0ecc0b6a59cdb265f6017f348079597fb630ee04
Nadia khaled <d703403@gmail.com>
2025-07-15 02:33:55 +0300

    done repo impl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -30,9 +30,23 @@ class HomeRepoImpl implements HomeRepo {
     }}
 
     @override
-    Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-      // TODO: implement fetchFeaturedBooks
-      throw UnimplementedError();
+    Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks()async {
+ try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&q=subject:programming");
+
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioExceptionError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
     }
   }
 
