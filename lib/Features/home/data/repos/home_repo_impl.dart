6937515e0462fe5d6f1e877812c8003088a33799
Nadia khaled <d703403@gmail.com>
2025-07-15 01:16:59 +0300

    create ServerFailure

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -22,13 +23,16 @@ class HomeRepoImpl implements HomeRepo {
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioExceptionError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }}
+
+    @override
+    Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+      // TODO: implement fetchFeaturedBooks
+      throw UnimplementedError();
     }
   }
 
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
-  }
-}
