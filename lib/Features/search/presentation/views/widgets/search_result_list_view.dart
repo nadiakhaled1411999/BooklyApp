3b985c83ac0ae97d9dc636a00ea9dcfebc247fd6
Nadia khaled <d703403@gmail.com>
2025-07-25 01:55:43 +0300

    add Name app

--- lib/Features/search/presentation/views/widgets/search_result_list_view.dart
+++ lib/Features/search/presentation/views/widgets/search_result_list_view.dart
@@ -5,7 +5,7 @@ import 'package:bookly_app/core/utils/widgets/custom_error_widget.dart';
 import 'package:bookly_app/core/utils/widgets/custom_loading_list_view/vertical_loading_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lottie/lottie.dart';
+
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
